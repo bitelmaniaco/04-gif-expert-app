b0c5decaf9a8f36bca7d23fa10ca8ce956c4c88b
bitelmaniaco <m.martindominguez@icloud.com>
2022-05-10 09:15:24 +0200

    ultima version

---
+++ src/GifGridItem.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { shallow } from 'enzyme';
+import GifGridItem from './components/GifGridItem';
+
+describe( () =>{
+
+
+
+
+
+
+})
